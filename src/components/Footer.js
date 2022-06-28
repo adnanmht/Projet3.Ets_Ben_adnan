2164ca12420e8e61e13351b5c46b364464084473
mahamat <mahamatadnan@gmail.com>
2022-06-28 08:38:22 +0200

    "footer css"

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import { NavLink } from "react-router-dom";
+import "./footer.css";
 
-const Footer = () => {
+export default function Footer() {
   return (
-    <div>
-      footerC
+    <div className="main-footer">
+      <div className="container">
+        <NavLink className={"content"} to="/ Mentions légales">
+        Mentions légales | Pretection des données | CGV | Conditions des
+          offres |
+        </NavLink>
+        <NavLink className={"content"} to="/Contact">
+          Contact: 0695230882 
+          mail: mahamatadnan@gmail.com
+          Plus d’inspiration 
+        </NavLink>
+      </div>
+      <div className="logo-content">
+        <div className="webName">
+          Plus d’inspiration 
+        </div>
+      
+      </div>
     </div>
   );
-};
-
-export default Footer;
+}
