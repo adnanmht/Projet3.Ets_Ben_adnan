7692b84facb3e3ecb5a6790362473655947e7eb0
mahamat <mahamatadnan@gmail.com>
2022-06-28 11:49:06 +0200

    Homme.css

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,7 +1,20 @@
 import React from "react";
+import "./Home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="Titre">
+      <div>BIENVENUE À L’ETS BEN ADNAN</div>
+      <div>
+        <p>Vente en Gros et detail</p>
+        <p>
+          Vous etes commerçant l’Ets Ben-Adnan vous ogffres la possibilités de
+          payé en 2 fois 3 fois et 4 fois sans frais
+        </p>
+        <p>DECOUVREZ NOS DIFFERENTS ARTICLES</p>
+      </div>{" "}
+    </div>
+  );
 };
 
 export default Home;
